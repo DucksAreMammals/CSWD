8d0c6297a9ed07fc8f497a147c678b2f478a5846
DucksAreMammals <bjornlucas@protonmail.com>
2025-02-18 09:27:13 -0600

    Finished lab 2

---
+++ ObjectBasics/objects.js
@@ -0,0 +1,45 @@
+// Part 2
+console.log("Part 2");
+
+// Calculate the circumference of a circle with radius 5
+console.log(Math.PI * 2 * 5);
+
+// Absolute value of -10
+console.log(Math.abs(-10));
+
+// Floor of 3.9
+console.log(Math.floor(3.9));
+
+// Ceiling of 4.2
+console.log(Math.ceil(4.2));
+
+// Random number
+console.log(Math.random());
+
+// Part 3
+console.log("Part 3");
+
+// Print today's date
+let date = new Date();
+console.log(date.toDateString());
+
+// Part 4
+console.log("Part 4");
+
+// Convert to uppercase
+console.log("hello world".toUpperCase());
+
+// Take the first 5 letters of string
+console.log("hello world".slice(0, 5));
+
+// Check's if "world" is in string
+console.log("hello world".includes("world"));
+
+// Part 5
+console.log("Part 5");
+
+// Rounds to 2 decimal places.
+console.log((123.456).toFixed(2));
+
+// Converts to integer
+console.log((123.456).toFixed(0));
